ece186d5c5588eca34b2f5aeaaef31e946ed2995
Rus1IT <ruslankoldasov1@gmail.com>
2025-10-02 23:16:50 +0500

    Initializing spring cloud gateway

---
+++ src/main/java/com/rus1it/springgatewaycashpilot/SpringGatewayCashpilotApplication.java
@@ -0,0 +1,13 @@
+package com.rus1it.springgatewaycashpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayCashpilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayCashpilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rus1it/springgatewaycashpilot/SpringGatewayCashpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rus1it.springgatewaycashpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayCashpilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
